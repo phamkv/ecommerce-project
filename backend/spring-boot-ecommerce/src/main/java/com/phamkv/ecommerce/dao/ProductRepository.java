d8e03bb20131950c5128ac8363c5e8686525a389
Kevin Pham <hainamkevin.pham@gmail.com>
2022-05-01 17:54:06 +0200

    Set up Spring Boot backend
    Set up database table and connect with Spring Boot. Make REST of Product and ProductClass to read-only.

---
+++ backend/spring-boot-ecommerce/src/main/java/com/phamkv/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.phamkv.ecommerce.dao;
+
+import com.phamkv.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
